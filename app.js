a5c3b2e9f072dec376765bea751ac572c33fe51f
theelgreco <stelios.kakoliris@yahoo.com>
2023-06-24 18:26:47 +0100

    added streaming link

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const cors = require("cors");
 const PORT = process.env.PORT || 4040;
 const axios = require("axios");
 
-const getImageResults = require("./function.js");
+const { getImageResults, getStreamingLink } = require("./function.js");
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/:actor", (req, res) => {
+app.get("/image/:actor", (req, res) => {
   const { actor } = req.params;
   const split = actor.split("&");
   let arr = [];
@@ -29,6 +29,14 @@ app.get("/:actor", (req, res) => {
           res.status(200).send(arr);
         }
       });
+  });
+});
+
+app.get("/search/link/:search_term", (req, res) => {
+  const { search_term } = req.params;
+  getStreamingLink(search_term).then((url) => {
+    console.dir(url);
+    res.status(200).send(url);
   });
 });
 
